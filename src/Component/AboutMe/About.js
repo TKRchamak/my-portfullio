eabf96e7ef5abe136acff30e54436beecea653e5
Tonmoy Kumar Roy <tkrchamak9813467@gmail.com>
2021-05-02 00:25:25 +0600

    get sms by gmail

--- src/Component/AboutMe/About.js
+++ src/Component/AboutMe/About.js
@@ -8,14 +8,14 @@ const About = () => {
         <section className="aboutStyle">
             <div className="container">
                 <div className="row d-flex">
-                    <div className="figure col-6">
+                    <div className="figure col-6 pt-5">
                         <img src={me} className="figure-img img-fluid rounded" alt="" />
                     </div>
-                    <div className="col-6 p-5 mt-5">
+                    <div className="col-6 p-5">
                         <h3>Hi, I am </h3>
                         <h1>Tonmoy Kumar Roy</h1>
-                        <p style={{color:"gray", fontSize:"20px"}}>I work as interface and <span style={{color:"#3BAFAA", fontWeight:'bolder'}}>front end developer.</span> <br /> I have passion for pixel perfect, minimal and easy to use interfaces.</p>
-                        <button  className="btn btn-style mt-5">Contact Me</button>
+                        <p style={{color:"gray", fontSize:"20px"}}>I work as interface and <span className="markWord">front end developer.</span> <br /> I have passion for pixel perfect, minimal and easy to use interfaces.</p>
+                        <p>If you want to know more about me or my work, or if you would just like to say hello, send me a message. I'd love to hear from you.</p>
                         <Email></Email>
                     </div>
                 </div>
--- src/Component/Mail/Email.js
+++ src/Component/Mail/Email.js
@@ -5,23 +5,32 @@ const Email = () => {
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+        emailjs.sendForm('service_nhj5uyp', 'template_vrq4fnf', e.target, 'user_klVnBJJwkoOowH6j36kkx')
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
                 console.log(error.text);
             })
+            e.target.reset();
     };
     return (
+        
         <form className="contact-form" onSubmit={sendEmail}>
             <input type="hidden" name="contact_number" />
-            <label>Name</label>
-            <input type="text" name="user_name" />
-            <label>Email</label>
-            <input type="email" name="user_email" />
-            <label>Message</label>
-            <textarea name="message" />
-            <input type="submit" value="Send" />
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label markWord">Name</label>
+                <input type="text" name="user_name" class="form-control" id="exampleInputEmail1" />
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label markWord">Email</label>
+                <input type="email" name="user_email" class="form-control" id="exampleInputEmail1" />
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label markWord">Message</label>
+                <textarea name="message" class="form-control" id="exampleInputEmail1" />
+            </div>
+            <input className="btn btn-style" type="submit" value="Send" />
         </form>
     );
 };
